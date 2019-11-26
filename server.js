869c9702b9f94f198d9cf2464cbebce20319b237
noxolica <angelokn14@gmail.com>
2019-11-26 13:29:55 -0800

    changed units to the metric (si) system

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ app.use(express.static("public"))
 
 // we only have one endpoint
 app.post("/weather", (req, res) => {
-  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=si`
   axios({
     url: url,
     responseType: 'json'
   }).then(data => res.json(data.data.currently))
 })
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
