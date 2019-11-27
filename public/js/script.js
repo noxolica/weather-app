d4e5123aa1558f961b0d78d6bf7cbe8898069cdc
noxolica <angelokn14@gmail.com>
2019-11-27 02:37:04 -0800

    added a loading spinner to the input while data is being feitched

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,18 @@
-const searchElement = document.querySelector('[data-search-city]')
+const inputBox = document.querySelector("[data-search-city]")
+const inputContainer = document.querySelector(".input-container")
 const options = {
   types: ["(cities)"]
 }
 
-const searchBox = new google.maps.places.Autocomplete(searchElement, options)
+const searchBox = new google.maps.places.Autocomplete(inputBox, options)
 
 google.maps.event.addListener(searchBox, "place_changed", () => {
   const place = searchBox.getPlace()
   if (place === null) {
     return
   }
+
+  inputContainer.classList.add("is-loading")
 
   const latitude = place.geometry.location.lat()
   const longitude = place.geometry.location.lng()
@@ -26,6 +29,8 @@ google.maps.event.addListener(searchBox, "place_changed", () => {
     })
   }).then(res => res.json()).then(data => {
     updateDisplay(data, place.name)
+    inputContainer.classList.remove("is-loading")
+    inputBox.value = ""
   })
 })
 
